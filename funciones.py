779c62c959a7dfbfd7927fb768edcd05d669c3a9
Mauricio Oyarzo <cmom1702@gmail.com>
2025-04-04 17:15:04 -0300

    Archivos Iniciales

---
+++ funciones.py
@@ -0,0 +1,19 @@
+define function1()
+    print("Function 1 executed")
+end function1
+
+define function2()
+    print("Function 2 executed")
+end function2
+
+define function3()
+    print("Function 3 executed")
+end function3
+
+define functionA()
+    print("Function A executed")
+end functionA
+
+define functionC()
+    print("Function C executed")
+end functionC
